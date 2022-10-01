7f6a47aafe7db73021f41d787580c5e8daeda3c9
jasonttnguyen <67293576+jasonttnguyen@users.noreply.github.com>
2022-10-01 17:41:04 -0400

    fixed ml, because as you give ftm/fga to the model, it can just calculate the points

--- src/backend/PlayerStatPredict.py
+++ src/backend/PlayerStatPredict.py
@@ -18,7 +18,7 @@ playerData = playerData[playerData["MP"].str.contains("Did Not Play") == False]
 playerData = playerData[playerData["MP"].str.contains("Did Not Dress") == False]
 playerData = playerData[playerData["FT%"].str.contains("NaN") == False]  #need to shift the games played/rnk down
 
-playerData = playerData[["MP","FG","FGA","3P","3PA","FT","FTA","TRB","AST","STL","BLK","TOV","PTS","+/-"]]
+playerData = playerData[["MP","FGA","TRB","AST","STL","BLK","TOV","PTS","+/-"]]
 
 def seconder(x):  #get seconds for minutes played
 	mins, secs = map(float, x.split(':'))
@@ -33,21 +33,21 @@ x = numpy.array(playerData.drop([predictionVar],1))  #creates an array of arrays
 y = numpy.array(playerData[predictionVar])           #creates an array of the wanted variable
 
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1 )  #trains model with 90 percent of data and test on 10% of data
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.05 )  #trains model with 95 percent of data and test on 5% of data
 
 
 linear = linear_model.LinearRegression()  #choosing linear regression as all stats are correalated 
 
 linear.fit(x_train,y_train)  #fit model with the training data
 accu = linear.score(x_test,y_test)  # used to print accuracy score
+print("accuracy is: " + str(accu))
 
 
 predictions = linear.predict(x_test)  #predict using x_test, which is randomized by sklearn
 print("\n Given that in a certain game, player achieves the following stats: ")
-print("MP: " + str(x_test[1][0]) + " FG: " + str(x_test[1][1]) + " FGA:" + str(x_test[1][2]) + " 3P: " + str(x_test[1][3]) + " 3PA: " + str(x_test[1][4]))
-print("FT: " + str(x_test[1][5]) + " FTA: " + str(x_test[1][6]) + " TRB: " + str(x_test[1][7]))
-print("AST: " + str(x_test[1][8]) + " STL: " + str(x_test[1][9]) + " BLK:" + str(x_test[1][10]) + " TOV: " + str(x_test[1][11]))
-print("+/-: " + str(x_test[1][12]) )
+print("MP: " + str(x_test[1][0]) + " FGA: "+ str(x_test[1][1]) +  " TRB: " + str(x_test[1][2]))
+print("AST: " + str(x_test[1][3]) + " STL: " + str(x_test[1][4]) + " BLK:" + str(x_test[1][5]) + " TOV: " + str(x_test[1][6]))
+print("+/-: " + str(x_test[1][7]) )
 userGuess=input("What do you think the player's point score is?: ")
 print("The machine learning model predicted: " + str(predictions[1]))
 print("The actual point score is: " + y_test[1])
