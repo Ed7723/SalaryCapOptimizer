2cc42598df81461c5b8e8ea8e84cd823b37f36db
jasonttnguyen <67293576+jasonttnguyen@users.noreply.github.com>
2022-10-01 14:23:28 -0400

    Update PlayerStatPredict.py

--- src/backend/PlayerStatPredict.py
+++ src/backend/PlayerStatPredict.py
@@ -5,7 +5,7 @@ from sklearn import linear_model
 from datetime import timedelta
 
 
-#This program predicts the stats of the current star raptor players as of Oct 30, 2019 (using linear regression algorithm)
+
 #This formats all the data so the most recent game is on the right, and last played game is on the left, as well as formatting the data given so it is easier to work with
 playerData = pandas.read_csv("vanvleet2021.csv")
 
@@ -13,18 +13,20 @@ playerData = playerData[playerData["MP"].str.contains("Inactive") == False]
 playerData = playerData[playerData["MP"].str.contains("Did Not Play") == False]
 playerData = playerData[playerData["MP"].str.contains("Did Not Dress") == False]
 playerData = playerData[playerData["FT%"].str.contains("NaN") == False]
-playerData = playerData[["MP","FG","FGA","FG%","3P","3PA","3P%","FT","FTA","FT%","ORB","DRB","TRB","AST","STL","BLK","TOV","PF","PTS","+/-"]]
+
+playerData = playerData[["MP","FG","FGA","3P","3PA","FT","FTA","TRB","AST","STL","BLK","TOV","PTS"]]
 
 def seconder(x):
 	mins, secs = map(float, x.split(':'))
 	td = timedelta(minutes=mins, seconds=secs)
 	return td.total_seconds()
-playerData['MP'] = playerData['MP'].apply(seconder)
+playerData['MP'] = playerData['MP'].apply(seconder) #converted to actually seconds played
 
 print(playerData)
 
 
-PTSpredict = "PTS"
+PTSpredict = "PTS" #what we are predicting
+ 
 x = numpy.array(playerData.drop([PTSpredict],1))
 y = numpy.array(playerData[PTSpredict])
 
